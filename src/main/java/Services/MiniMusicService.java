c672ed796c0c786b3ed28a2952af0d83bbf5451d
EvgeniyKazancev <ekazancev777@gmail.com>
2023-07-21 21:19:03 +0300

    refresh

--- src/main/java/Services/MiniMusicService.java
+++ src/main/java/Services/MiniMusicService.java
@@ -2,13 +2,16 @@ package Services;
 
 import Interface.Service;
 
+import javax.sound.midi.*;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MiniMusicService implements Service {
     MyDrawPanel myPanel;
-    public JPanel getGuiPanel(){
+
+    public JPanel getGuiPanel() {
         JPanel mainPanel = new JPanel();
         myPanel = new MyDrawPanel();
         JButton playItButton = new JButton("Play it");
@@ -17,10 +20,74 @@ public class MiniMusicService implements Service {
         mainPanel.add(playItButton);
         return mainPanel;
     }
-    public class PlayItListener implements ActionListener{
-        public void actionPerformed(ActionEvent ev){
-            try {
 
+    public class PlayItListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                Sequencer sequencer = MidiSystem.getSequencer();
+                sequencer.open();
+                sequencer.addControllerEventListener(myPanel, new int[]{127});
+                Sequence seq = new Sequence(Sequence.PPQ, 4);
+                Track track = seq.createTrack();
+
+                for (int i = 0; i < 100; i += 4) {
+
+                    int rNum = (int) ((Math.random() * 50) + 1);
+                    if (rNum < 38) {
+                        track.add(makeEvent(144, 1, rNum, 100, i));
+                        track.add(makeEvent(176, 1, 127, 0, i));
+                        track.add(makeEvent(128, 1, rNum, 100, i + 2));
+
+                    }
+                }
+                sequencer.setSequence(seq);
+                sequencer.start();
+                sequencer.setTempoInBPM(220);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+        }
+        return event;
+    }
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+        boolean msg = false;
+
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+
+        public Dimension getPreferredSize() {
+            return new Dimension(300, 300);
+        }
+
+        public void paintComponent(Graphics g) {
+            if (msg) {
+                Graphics2D g2 = (Graphics2D) g;
+
+                int r = (int) (Math.random() * 250);
+                int gr = (int) (Math.random() * 250);
+                int b = (int) (Math.random() * 250);
+
+                g.setColor(new Color(r, gr, b));
+                int ht = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 10);
+
+                g.fillRect(x, y, ht, width);
+                msg = false;
             }
         }
     }
